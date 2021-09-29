58ee688313ed136a01a9efc54046de1c63fa8511
Andrew soares <andrewsoares347@gmail.com>
2021-09-29 11:05:28 -0300

    versao 1.1

--- projeto-vue/src/views/Time.vue
+++ projeto-vue/src/views/Time.vue
@@ -3,7 +3,7 @@
 
       <h1 class="text-center"> Vasco da gama</h1>
       <v-img src="../img/vasco.jpg" alt=""> </v-img>
-      <p class="mt-2">Nenê é o expoente do bom momento do Vasco, mas a melhora de rendimento de jogadores que andavam em baixa vem colaborando com o desempenho do time. Desde a chegada de Fernando Diniz, nomes contestados e outros que pouco jogavam ganharam espaço e elogios. Os casos de Marquinhos Gabriel, Morato e Ricardo Graça são emblemáticos.</p>
+      <p class="mt-2 mb-2">Nenê é o expoente do bom momento do Vasco, mas a melhora de rendimento de jogadores que andavam em baixa vem colaborando com o desempenho do time. Desde a chegada de Fernando Diniz, nomes contestados e outros que pouco jogavam ganharam espaço e elogios. Os casos de Marquinhos Gabriel, Morato e Ricardo Graça são emblemáticos.</p>
       <v-btn
   elevation="2"
   loading
